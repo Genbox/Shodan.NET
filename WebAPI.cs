7fec26a576d1b4a1455bf8279bbdc0ee86ff0796
Ian Qvist <qvistian@gmail.com>
2012-07-15 08:03:56 +0200

    Added support for searching and downloading exploitdb and msf modules/exploits

--- WebAPI.cs
+++ WebAPI.cs
@@ -4,49 +4,44 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Text;
 using System.Web;
 using System.Web.Script.Serialization;
 using System.Globalization;
 
 namespace Shodan
 {
-    public class WebAPI
+    public class Shodan
     {
-        private readonly string API_KEY;
-        private const string BASE_URL = "http://beta.shodanhq.com/api/";
-        private JavaScriptSerializer json_parser = new JavaScriptSerializer();
-        private WebClient web_client = new WebClient();
+        private const string BaseUrl = "http://beta.shodanhq.com/api/";
+        private readonly string _apiKey;
+        private JavaScriptSerializer _jsonParser = new JavaScriptSerializer();
+        private WebClient _webClient = new WebClient();
 
-        /// <summary>
-        /// Initialize the Shodan WebAPI object.
-        /// </summary>
-        /// <param name="api_key">The Shodan API key for your account.</param>
-        public WebAPI(string apiKey)
+        public Shodan(string apiKey)
         {
-            API_KEY = apiKey;
+            _apiKey = apiKey;
         }
 
         /// <summary>
-        /// Get all the information Shodan has on the IP.
+        ///  Get all the information Shodan has on the IP.
         /// </summary>
         /// <param name="ip">IP of the computer to look up</param>
         /// <returns>A Host object with the banners and location information.</returns>
         public Host GetHost(IPAddress ip)
         {
-            string str_ip = ip.ToString();
+            string strIp = ip.ToString();
 
             // Send the request
             Dictionary<string, string> args = new Dictionary<string, string>();
-            args["ip"] = str_ip;
-            Dictionary<string, object> res_dict = SendRequest("host", args);
+            args["ip"] = strIp;
+            Dictionary<string, object> resDict = SendRequest("host", args);
 
-            Host host = new Host(res_dict);
+            Host host = new Host(resDict);
             return host;
         }
 
         /// <summary>
-        /// Get all the information Shodan has on the IP (given as a string).
+        ///  Get all the information Shodan has on the IP (given as a string).
         /// </summary>
         /// <param name="ip">IP of the computer to look up</param>
         /// <returns>A Host object with the banners and location information.</returns>
@@ -56,37 +51,109 @@ namespace Shodan
         }
 
         /// <summary>
-        /// Search the Shodan search engine for computers matching the given search criteria.
+        ///  Search the Shodan search engine for computers matching the given search criteria.
         /// </summary>
-        /// <param name="query">The search query for Shodan; identical syntax to the website.</param>
-        /// <param name="offset">The starting position for the search cursor. Only for enterprise customers.</param>
-        /// <param name="limit">The number of hosts to return (max. 100) per search query. Only for enterprise customers.</param>
-        /// <returns>A SearchResult object that contains a List of Hosts matching the query and the total number of results found.</returns>
-        public SearchResult Search(string query, int offset=0, int limit=50)
+        /// <param name="query">The search query for Shodan; identical syntax to the website. </param>
+        /// <param name="offset">The starting position for the search cursor.</param>
+        /// <param name="limit">The number of hosts to return per search query.</param>
+        /// <returns> A SearchResult object that contains a List of Hosts matching the query and the total number of results found. </returns>
+        public List<Host> Search(string query, int offset = 0, int limit = 100)
         {
             Dictionary<string, string> args = new Dictionary<string, string>();
             args["q"] = query;
             args["o"] = offset.ToString();
             args["l"] = limit.ToString();
-            Dictionary<string, object> res_dict = SendRequest("search", args);
+            Dictionary<string, object> resDict = SendRequest("search", args);
 
-            SearchResult result = new SearchResult(res_dict);
-            return result;
+            List<Host> hosts = new List<Host>(resDict.Count);
+
+            ArrayList arrayList = (ArrayList)resDict["matches"];
+            foreach (Dictionary<string, object> item in arrayList)
+            {
+                hosts.Add(new Host(item, true));
+            }
+
+            return hosts;
+        }
+
+        public List<Exploit> SearchExploits(string query, string author = "", string platform = "", int port = 0, string type = "")
+        {
+            Dictionary<string, string> args = new Dictionary<string, string>();
+            args["q"] = query;
+
+            if (!string.IsNullOrEmpty(author))
+                args["author"] = author;
+
+            if (!string.IsNullOrEmpty(platform))
+                args["platform"] = platform;
+
+            if (port >= 1)
+                args["port"] = port.ToString();
+
+            if (!string.IsNullOrEmpty(type))
+                args["type"] = type;
+
+            Dictionary<string, object> resDict = SendRequest("exploitdb/search", args);
+            List<Exploit> exploits = new List<Exploit>(resDict.Count);
+
+            ArrayList arrayList = (ArrayList)resDict["matches"];
+            foreach (Dictionary<string, object> item in arrayList)
+            {
+                exploits.Add(new Exploit(item));
+            }
+
+            return exploits;
+        }
+
+        public DataResponse DownloadExploit(int id)
+        {
+            Dictionary<string, string> args = new Dictionary<string, string>();
+            args["id"] = id.ToString();
+            Dictionary<string, object> resDict = SendRequest("exploitdb/download", args);
+
+            DataResponse exploit = new DataResponse(resDict);
+            return exploit;
+        }
+
+        public List<MSFModule> SearchMSFModules(string query)
+        {
+            Dictionary<string, string> args = new Dictionary<string, string>();
+            args["q"] = query;
+
+            Dictionary<string, object> resDict = SendRequest("msf/search", args);
+            List<MSFModule> modules = new List<MSFModule>(resDict.Count);
+
+            ArrayList arrayList = (ArrayList)resDict["matches"];
+            foreach (Dictionary<string, object> item in arrayList)
+            {
+                modules.Add(new MSFModule(item));
+            }
+
+            return modules;
+        }
+
+        public DataResponse DownloadMSFModule(string id)
+        {
+            Dictionary<string, string> args = new Dictionary<string, string>();
+            args["id"] = id;
+            Dictionary<string, object> resDict = SendRequest("msf/download", args);
+
+            DataResponse module = new DataResponse(resDict);
+            return module;
         }
 
         /// <summary>
-        /// Internal wrapper function to send API requests.
+        ///  Internal wrapper function to send API requests.
         /// </summary>
-        /// <param name="api_func">The API function to call.</param>
+        /// <param name="apiFunc">The API function to call.</param>
         /// <param name="args">The arguments to pass to the given API function.</param>
-        /// <returns>A Dictionary<string, object> with the deserialized JSON information.</returns>
-        private Dictionary<string, object> SendRequest(string api_func, Dictionary<string, string> args)
+        private Dictionary<string, object> SendRequest(string apiFunc, Dictionary<string, string> args)
         {
             // Convert the arguments to a query string
-            string str_args = ToQuerystring(args);
+            string strArgs = ToQuerystring(args);
 
             // Send the request
-            Stream response = web_client.OpenRead(BASE_URL + api_func + str_args + "&key=" + API_KEY);
+            Stream response = _webClient.OpenRead(BaseUrl + apiFunc + strArgs + "&key=" + _apiKey);
 
             // Read the response into a string
             StreamReader reader = new StreamReader(response);
@@ -94,13 +161,11 @@ namespace Shodan
             reader.Close();
 
             // Turn the JSON string into a native dictionary object
-            Dictionary<string, object> result = json_parser.Deserialize<Dictionary<string, object>>(data);
+            Dictionary<string, object> result = _jsonParser.Deserialize<Dictionary<string, object>>(data);
 
             // Raise an exception if an error was returned
             if (result.ContainsKey("error"))
-            {
-                throw new System.ArgumentException((string)result["error"]);
-            }
+                throw new ArgumentException((string)result["error"]);
 
             return result;
         }
@@ -111,125 +176,100 @@ namespace Shodan
         }
     }
 
-    public class SearchResult
+    public class Exploit
     {
-        private int numResults; // total # of results
-        private List<Host> hosts;
-
-        public int NumResults { get { return numResults; } }
-        public List<Host> Hosts { get { return hosts; } }
-
-        public SearchResult(Dictionary<string, object> results)
+        public Exploit(Dictionary<string, object> result)
         {
-            // Get the total number of results
-            numResults = (int)results["total"];
+            CultureInfo provider = CultureInfo.InvariantCulture;
 
-            // Loop through the matches and create host entries
-            hosts = new List<Host>();
-            foreach (Dictionary<string, object> item in (ArrayList)results["matches"])
-            {
-                hosts.Add(new Host(item, true));
-            }
+            // Extract the info out of the host dictionary and put it in the local properties
+            Id = int.Parse(result["ip"].ToString());
+            Author = result["author"].ToString();
+            Date = DateTime.ParseExact((string)result["date"], "dd.MM.yyyy", provider);
+            Description = result["description"].ToString();
+            Platform = result["platform"].ToString();
+            Port = int.Parse(result["port"].ToString());
+            Type = result["type"].ToString();
         }
+
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Platform { get; set; }
+        public int Port { get; set; }
+        public string Type { get; set; }
     }
 
     public class ServiceBanner
     {
-        private int port;
-        private string banner;
-        private DateTime timestamp;
-
-        public int Port { get { return port; } }
-        public string Banner { get { return banner; } }
-        public DateTime Timestamp { get { return timestamp; } }
-
-        public ServiceBanner(int arg_port, string arg_banner, DateTime arg_timestamp)
+        public ServiceBanner(int argPort, string argBanner, DateTime argTimestamp)
         {
-            port = arg_port;
-            banner = arg_banner;
-            timestamp = arg_timestamp;
+            Port = argPort;
+            Banner = argBanner;
+            Timestamp = argTimestamp;
         }
+
+        public int Port { get; private set; }
+        public string Banner { get; private set; }
+        public DateTime Timestamp { get; private set; }
     }
 
     public class HostLocation
     {
-        private string country_code;
-        private string country_name;
-        private string city;
-        private double latitude;
-        private double longitude;
-
-        public string CountryCode { get { return country_code; } }
-        public string CountryName { get { return country_name; } }
-        public string City { get { return city; } }
-        public double Latitude { get { return latitude; } }
-        public double Longitude { get { return longitude; } }
-
         public HostLocation(Dictionary<string, object> host)
         {
             // Extract the info out of the host dictionary and put it in the local properties
             if (host.ContainsKey("country_name"))
-                country_name = (string)host["country_name"];
+                CountryName = (string)host["country_name"];
 
             if (host.ContainsKey("country_code"))
-                country_code = (string)host["country_code"];
+                CountryCode = (string)host["country_code"];
 
             if (host.ContainsKey("city"))
-                city = (string)host["city"];
+                City = (string)host["city"];
 
             if (host.ContainsKey("latitude"))
             {
-                latitude = (double)((decimal)host["latitude"]);
-                longitude = (double)((decimal)host["longitude"]);
+                Latitude = (double)((decimal)host["latitude"]);
+                Longitude = (double)((decimal)host["longitude"]);
             }
         }
 
+        public string CountryCode { get; private set; }
+        public string CountryName { get; private set; }
+        public string City { get; private set; }
+        public double Latitude { get; private set; }
+        public double Longitude { get; private set; }
+
         /// <summary>
-        /// Check whether there are valid coordinates available for this location.
+        ///   Check whether there are valid coordinates available for this location.
         /// </summary>
-        /// <returns>true if there are latitude/ longitude coordinates, false otherwise.</returns>
+        /// <returns> true if there are latitude/ longitude coordinates, false otherwise. </returns>
         public Boolean HasCoordinates()
         {
             if (Latitude != 0 && Longitude != 0)
-            {
                 return true;
-            }
+
             return false;
         }
     }
 
     public class Host
     {
-        /*
-         * Setup the properties
-         */
-        private List<ServiceBanner> banners;
-        private IPAddress ip = IPAddress.None;
-        private List<string> hostnames;
-        private HostLocation location = null;
-        private Boolean simple = false;
-
-        public List<ServiceBanner> Banners { get { return banners; } }
-        public IPAddress IP { get { return ip; } }
-        public List<string> Hostnames { get { return hostnames; } }
-        public HostLocation Location { get { return location; } }
-
-        // Used to differentiate between hosts from Search() results and direct GetHost() queries
-        public Boolean IsSimple { get { return simple; } }
-
-        public Host(Dictionary<string, object> host, Boolean simple=false)
+        public Host(Dictionary<string, object> host, bool simple = false)
         {
             CultureInfo provider = CultureInfo.InvariantCulture;
 
             // Extract the info out of the host dictionary and put it in the local properties
-            ip = IPAddress.Parse(host["ip"].ToString());
+            IP = IPAddress.Parse(host["ip"].ToString());
 
             // Hostnames
             ArrayList tmp = (ArrayList)host["hostnames"];
-            hostnames = tmp.Cast<string>().ToList();
+            Hostnames = tmp.Cast<string>().ToList();
 
             // Banners
-            banners = new List<ServiceBanner>();
+            Banners = new List<ServiceBanner>();
 
             if (host["data"] is ArrayList)
             {
@@ -237,17 +277,87 @@ namespace Shodan
                 foreach (Dictionary<string, object> data in tmp)
                 {
                     DateTime timestamp = DateTime.ParseExact((string)data["timestamp"], "dd.MM.yyyy", provider);
-                    banners.Add(new ServiceBanner((int)data["port"], (string)data["banner"], (DateTime)timestamp));
+                    Banners.Add(new ServiceBanner((int)data["port"], (string)data["banner"], timestamp));
                 }
             }
             else if (host["data"] is string)
             {
                 DateTime timestamp = DateTime.ParseExact((string)host["updated"], "dd.MM.yyyy", provider);
-                banners.Add(new ServiceBanner((int)host["port"], (string)host["data"], (DateTime)timestamp));
+                Banners.Add(new ServiceBanner((int)host["port"], (string)host["data"], timestamp));
             }
 
             // Location
-            location = new HostLocation(host);
+            Location = new HostLocation(host);
+
+            IsSimple = simple;
         }
+
+        public List<ServiceBanner> Banners { get; private set; }
+        public IPAddress IP { get; private set; }
+        public List<string> Hostnames { get; private set; }
+        public HostLocation Location { get; private set; }
+
+        /// <summary>
+        /// Used to differentiate between hosts from Search() results and direct GetHost() queries
+        /// </summary>
+        public bool IsSimple { get; private set; }
+    }
+
+    public class DataResponse
+    {
+        public DataResponse(Dictionary<string, object> data)
+        {
+            Data = data["data"].ToString();
+            ContentType = data["content-type"].ToString();
+            Filename = data["filename"].ToString();
+        }
+
+        public string Data { get; set; }
+        public string ContentType { get; set; }
+        public string Filename { get; set; }
+    }
+
+    public class MSFModule
+    {
+        public MSFModule(Dictionary<string, object> data)
+        {
+            Alias = data["alias"].ToString();
+            Arch = data["arch"].ToString();
+
+            ArrayList authors = data["authors"] as ArrayList;
+            if (authors != null)
+                Authors = new List<string>(authors.Cast<string>());
+
+            Description = data["description"].ToString();
+            Fullname = data["fullname"].ToString();
+            Name = data["name"].ToString();
+
+            ArrayList platforms = data["platforms"] as ArrayList;
+            if (platforms != null)
+                Platforms = new List<string>(platforms.Cast<string>());
+
+            Privileged = bool.Parse(data["privileged"].ToString());
+            Rank = data["rank"].ToString();
+
+            ArrayList references = data["references"] as ArrayList;
+            if (references != null)
+                References = new List<string>(references.Cast<string>());
+
+            Type = data["type"].ToString();
+            Version = data["version"].ToString();
+        }
+
+        public string Alias { get; set; }
+        public string Arch { get; set; }
+        public List<string> Authors { get; set; }
+        public string Description { get; set; }
+        public string Fullname { get; set; }
+        public string Name { get; set; }
+        public List<string> Platforms { get; set; }
+        public bool Privileged { get; set; }
+        public string Rank { get; set; }
+        public List<string> References { get; set; }
+        public string Type { get; set; }
+        public string Version { get; set; }
     }
 }
